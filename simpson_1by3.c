be0bd2da8b70c2c1cec28e8e8b0473e440fa087f
Chetan Malhotra <chetanalawaysmalhotra@hotmail.com>
2016-11-09 00:59:42 +0530

    Simpson's 1/3 rule

---
+++ simpson_1by3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+float f(float x)
+{
+	return(1/(1+x));
+}
+
+int main()
+{
+	int i,n;
+float a,b,h,I,sum;
+printf("Enter the lower limit: ");
+scanf("%f",&a);
+printf("\nEnter the upper limit: ");
+scanf("%f",&b);
+printf("\nEnter the number of sub-intervals: ");
+scanf("%d",&n);
+h=(b-a)/n;
+sum=f(a)+f(b);
+for(i=1;i<n-1;i++)
+{
+	if(i%2==0)
+	{
+	sum+=(2*f(a+i*h));
+}
+else
+{
+	sum+=(4*f(a+i*h));
+}
+}
+ 	I=(h/3)*sum;
+ 	printf("\n\nThe value of I is: %f",I);
+ 	return 0;
+}
